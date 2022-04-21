91fdc88aa715ea36417f3b53067057720e92ab69
BakaShanNei <shannei@elyart.cn>
2022-04-21 15:55:44 +0800

    4月21日 1.0完工

--- program.cs
+++ program.cs
@@ -12,11 +12,9 @@
             var NameList = System.IO.File.ReadAllLines(NameListPath);  //将名单文件的内容存储到这个数组中
 
             var GotNameFile = System.Environment.CurrentDirectory + "\\已抽取名单.txt";  //读入已抽取名单文件
-            var GotNameList = System.IO.File.ReadAllLines(GotNameFile);  //将已抽取名单文件的内容存储到这个数组中
-
-            //设置一些基本变量
-            var GetTicketList = new List<string>();  //存储已经获得的票
-            var GetNameList = new List<string>();  //存储已经获得的名单
+            var GotNameListFromFile = System.IO.File.ReadAllLines(GotNameFile);  //将已抽取名单文件的内容存储到这个数组中
+            var GotNameList = new string[14];  //临时变量，存储本次已抽取名单
+            var GotTicketList = new string[14];  //临时变量，存储已经获得的票
 
             string[] TicketName = {"疯味大鸡腿","章鱼小丸子","炸全鸡"};  //存储票的名字
 
@@ -48,8 +46,6 @@
             Console.WriteLine("[输入] 输入你手中炸全鸡的数量",Console.ForegroundColor = ConsoleColor.Green);
             var ChickenKababCount = int.Parse(Console.ReadLine());  
 
-            Thread.Sleep(1000);  //等待1秒
-
             //Part 2 抽取夜宵券
             var TicketCount = ChickenTikkaCount + ShrimpKababCount + ChickenKababCount;  //统计夜宵券的数量
             var NameListCount = NameList.Length;  //统计名单的数量
@@ -58,40 +54,106 @@
             var TempShrimpKababCount = 0;  //临时变量，用于记录已抽取的章鱼小丸子的数量
             var TempChickenKababCount = 0;  //临时变量，用于记录已抽取的炸全鸡的数量
             
+            var TotleTicketCount = TempChickenKababCount + TempShrimpKababCount + TempChickenTikkaCount;  //统计已经抽取的夜宵券的数量
+            
+
             //开始抽取夜宵券
             Random RdTicket = new Random();  //创建随机数生成器
 
-            while(TempChickenKababCount + TempShrimpKababCount + TempChickenTikkaCount < TicketCount)
+            Array.Resize(ref GotTicketList, TicketCount);  //重新设置数组的大小
+            while(TotleTicketCount < TicketCount)
             {
-                int RdTicketType = RdTicket.Next(0,300);  //生成随机数，用于抽取夜宵券的类型
-                if(RdTicketType % 3 == 0 && TempChickenTikkaCount < ChickenTikkaCount)
+                for(int i = 0 ; i < GotTicketList.Length ; i ++)
                 {
-                    Console.WriteLine("[抽取] 抽取疯味大鸡腿夜宵券",Console.ForegroundColor = ConsoleColor.Red);
-                    TempChickenTikkaCount++;
-                }
-                else if(RdTicketType % 3 == 1 && TempShrimpKababCount < ShrimpKababCount)
-                {
-                    Console.WriteLine("[抽取] 抽取章鱼小丸子夜宵券",Console.ForegroundColor = ConsoleColor.Red);
-                    TempShrimpKababCount++;
-                }
-                else if(RdTicketType % 3 == 2 && TempChickenKababCount < ChickenKababCount)
-                {
-                    Console.WriteLine("[抽取] 抽取炸全鸡夜宵券",Console.ForegroundColor = ConsoleColor.Red);
-                    TempChickenKababCount++;
-                }
+                    int RdTicketType = RdTicket.Next(0,300);  //生成随机数，用于抽取夜宵券的类型
+                    if(RdTicketType % 3 == 0 && TempChickenTikkaCount < ChickenTikkaCount)
+                    {
+                        TempChickenTikkaCount++;
+                        Console.WriteLine("[抽取] 抽取了一张疯味大鸡腿的夜宵券",Console.ForegroundColor = ConsoleColor.Green);
+                        GotTicketList[i] = "疯味大鸡腿";
+                    }
+                    else if(RdTicketType % 3 == 1 && TempShrimpKababCount < ShrimpKababCount)
+                    {
+                        TempShrimpKababCount++;
+                        Console.WriteLine("[抽取] 抽取了一张章鱼小丸子的夜宵券",Console.ForegroundColor = ConsoleColor.Green);
+                        GotTicketList[i] = "章鱼小丸子";
+                    }
+                    else if(RdTicketType % 3 == 2 && TempChickenKababCount < ChickenKababCount)
+                    {
+                        TempChickenKababCount++;
+                        Console.WriteLine("[抽取] 抽取了一张炸全鸡的夜宵券",Console.ForegroundColor = ConsoleColor.Green);
+                        GotTicketList[i] = "炸全鸡";
+                    }
+                    else
+                    {
+                        Console.WriteLine("[抽取] 抽到了一张重复的票，重新抽取",Console.ForegroundColor = ConsoleColor.Yellow);
+                        i--;
+                    }
+
+                TotleTicketCount = TempChickenKababCount + TempShrimpKababCount + TempChickenTikkaCount;
+                Thread.Sleep(500);
+                } 
             }
 
+            Console.WriteLine("[信息] 夜宵券抽取完毕，共抽取{0}张夜宵券",TotleTicketCount,Console.ForegroundColor = ConsoleColor.White);  //输出抽取夜宵券的数量
+            
             //Part 3 抽人
             //读取已经抽过人的名单
-            if(GotNameList.Length > 0)
+            Random NameRd = new Random();  //创建随机数生成器
+            for(int i = 0 ; i < TicketCount ; i++)
             {
-                Console.WriteLine("[信息] 已经抽过人的名单：{0}", GotNameList.ToString(),Console.ForegroundColor = ConsoleColor.White);
-            }
-            else
-            {
-                Console.WriteLine("[信息] 已经抽过人的名单：{0}", "无",Console.ForegroundColor = ConsoleColor.White);
-            }
+                int NameRdType = NameRd.Next(0,NameListCount);  //生成随机数，用于抽取名单的下标
+                if(GotNameList.Contains(NameList[NameRdType]))
+                {
+                    Console.WriteLine("[调试] 当前抽到人名：{0}，该人名包含在已抽过人的本次抽取名单中，重新抽取",NameList[NameRdType],Console.ForegroundColor = ConsoleColor.Yellow);
+                    i--;
+                }
+                else if(GotNameListFromFile.Contains(NameList[NameRdType]))
+                {
+                    Console.WriteLine("[调试] 当前抽到人名：{0}，该人名包含在已抽过人的过去抽取名单文件中，重新抽取",NameList[NameRdType],Console.ForegroundColor = ConsoleColor.Yellow);
+                    i--;
+                }
+                else
+                {
+                    Array.Resize(ref GotNameList,GotNameList.Length + 1);
+                    GotNameList[i] = NameList[NameRdType];  //把抽取的名单添加到已抽取的名单列表中
+                    Console.WriteLine("[抽取] 当前抽取名单：{0}", NameList[NameRdType],Console.ForegroundColor = ConsoleColor.Green);  //输出抽取的名单
+                }
+
+                Thread.Sleep(500);  //等待1秒
+            }            
+
+            Console.WriteLine("[信息] 所有内容抽取完毕，5秒钟后输出本次抽取的结果");
+            
             Thread.Sleep(5000);  //等待5秒
+
+            Console.WriteLine("\n");
+            for(int i = 0 ; i < GotTicketList.Length - 2 ; i ++)
+            {
+                Console.WriteLine("[抽取] 已抽取的名单：{0}，对应的夜宵券：{1}", GotNameList[i],GotTicketList[i],Console.ForegroundColor = ConsoleColor.Yellow);
+            }
+
+
+            //Part 4 存储已抽取的名单
+            //将已抽取的名单写入文件
+            Console.WriteLine("[信息] 正在将已抽取的名单写入文件中");
+            string FilePath = System.Environment.CurrentDirectory + "\\" + "已抽取名单.txt";
+            FileStream fs = new FileStream(FilePath,FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            for(int i = 0 ; i < GotNameList.Length - 1 ; i ++)
+            {
+                sw.WriteLine(GotNameList[i]);
+            }
+            for(int i = 0 ; i < GotNameListFromFile.Length - 1 ; i ++)
+            {
+                sw.WriteLine(GotNameListFromFile[i]);
+            }
+            sw.Close();
+            fs.Close();
+
+            //结果写入完毕
+            Console.WriteLine("[信息] 已抽取的名单已经写入文件，5分钟后程序自动退出");
+            Thread.Sleep(300000);  //等待5分钟
 
         }
         //找不到文件
